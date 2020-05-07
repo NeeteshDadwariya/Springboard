0366b90d6cc22241936e109bee3a2783b20ba16f
Neetesh Dadwariya <Neetesh.Kumar@go-mmt.com>
2020-05-07 10:37:34 +0530

    Folder Refactoring

--- Python Work/My First Program.py
+++ Python Work/My First Program.py
@@ -1,4 +1,10 @@
-from __future__ import print_function
-import torch
-x = torch.rdvefefd(5, 3)
-print(x)
+import pandas as pd
+
+A = [(0, 1),
+     (1, 1)];
+new_df = df_A = pd.DataFrame(data=A);
+
+for i in range(0, 3):
+    new_df = new_df.dot(df_A);
+    print(str(i))
+    print(new_df)
